462f0b52471039a371449240bff53c0791f328fa
Jaime Sanchez <sanchezjaime@protonmail.com>
2020-09-14 21:33:19 -0700

    added routes and about.pug, index.pug, layout.pug, project.pug

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 const express = require('express');
-const data = require('data.json');
+//const data = require('data.json');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use('/static', express.static('public'));
+
+//routes
+//index route
+app.get('/', (req, res) => {
+  res.render('index', {data.projects});
+});
+//about route
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+//dynamic projects route
+app.get('/project/:id', (req, res) => {
+  res.render('project');
+})
+
+//event listener set to start server on port 3000
+app.listen(3000, () => {
+  console.log('This application is running on localhost:3000!')
+});
